37c1829661f801b4b5ee8801f5501d5bb8ff011c
Sumanth <sumanth@shoplocket.com>
2013-04-20 17:05:16 -0400

    Added moon images model

---
+++ app/models/moon_image.rb
@@ -0,0 +1,3 @@
+class MoonImage < ActiveRecord::Base
+  attr_accessible :kml_link, :taken_at
+end
---
+++ spec/models/moon_image_spec.rb
@@ -0,0 +1,5 @@
+require 'spec_helper'
+
+describe MoonImage do
+  pending "add some examples to (or delete) #{__FILE__}"
+end
