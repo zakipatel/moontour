0d4d9629f75dd2e31221c700d4c7d76f11512df1
Sumanth <sumanth@shoplocket.com>
2013-04-20 20:49:48 -0400

    Removed debugger

--- app/controllers/moon_images_controller.rb
+++ app/controllers/moon_images_controller.rb
@@ -1,6 +1,6 @@
 class MoonImagesController < ApplicationController
   def query_on_time
-    start_time = Time.at(params[:from].to_i) 
+    start_time = Time.at(params[:from].to_i)
     end_time = Time.at(params[:from].to_i + 3600)
     moon_images = MoonImage.taken_between(start_time, end_time)
     kml_links = moon_images.collect(&:kml_link)
@@ -13,9 +13,7 @@ class MoonImagesController < ApplicationController
   def query_on_coords
     lat = params[:lat]
     lon = params[:lon]
-
     moon_images = MoonImage.for_lat_lon(lat.to_f, lon.to_f, 3.5)
-    debugger
     kml_links = moon_images.collect(&:kml_link)
 
     respond_to do |format|
